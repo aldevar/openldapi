1628439be3c2efa9deb62fffb92e128e5492c5dc
aldevar <aldevar@alaben.net>
2017-02-01 22:09:51 +0100

    improve json rendering

--- webapp.py
+++ webapp.py
@@ -3,41 +3,17 @@
 from flask import Flask, jsonify, request
 import ldap
 import json
-import ConfigParser
+import getConfig
 
 app = Flask(__name__)
 
-#Get options from config file
-config = ConfigParser.ConfigParser()
-config.read("apildap.conf")
-def ConfigSectionMap(section):
-  dictopt = {}
-  options = config.options(section)
-  for option in options:
-    try:
-      dictopt[option] = config.get(section, option)
-      if dictopt[option] == -1:
-        DebugPrint("skip: {}".format(option))
-    except:
-      print("exception on {}".format(option))
-      dictopt[option] = None
-  print("toto")
-  return dictopt
-
-ldap_server = ConfigSectionMap('LdapServer')['server']
-ldap_port = ConfigSectionMap('LdapServer')['port']
-base_dn = ConfigSectionMap('LdapConfig')['base']
-user_ou = ConfigSectionMap('LdapConfig')['user']
-user_attr = ConfigSectionMap('LdapConfig')['user_attr']
-ldapcred = ConfigSectionMap('Credentials')['login']
-ldappass = ConfigSectionMap('Credentials')['password']
-
+#Endpoint returning every attributes for a given user
 @app.route('/api/v1/user/<username>/', methods=['GET'])
 def api_getuser(username):
-  connect = ldap.initialize('ldap://{0}:{1}'.format(ldap_server,ldap_port))
-  search_filter = "{}={}".format(user_attr,username)
+  connect = ldap.initialize('ldap://{0}:{1}'.format(getConfig.ldap_server,getConfig.ldap_port))
+  search_filter = "{}={}".format(getConfig.user_attr,username)
   try:
-    result = connect.search_s(user_ou,ldap.SCOPE_SUBTREE,search_filter)
+    result = connect.search_s(getConfig.user_ou,ldap.SCOPE_SUBTREE,search_filter)
     connect.unbind_s()
   except ldap.LDAPError as e:
     connect.unbind_s()
@@ -46,17 +22,18 @@ def api_getuser(username):
     for dn,entry in result:
       for key,value in entry.items():
         entry[key] = ",".join(value)
-    return jsonify("{{{}: {}}}".format(result[0][0],result[0][1]))
+#    return jsonify(result[0][0],result[0][1])
+    return jsonify(result[0])
 
+
+#Endpoint returning all the users
 @app.route('/api/v1/users/all/', methods=['GET'])
 def api_getalluser():
-  connect = ldap.initialize('ldap://{0}:{1}'.format(ldap_server,ldap_port))
-  connect.bind_s(ldapcred, ldappass)
-#  search_filter = "{}=*".format(user_attr)
+  connect = ldap.initialize('ldap://{0}:{1}'.format(getConfig.ldap_server,getConfig.ldap_port))
+  connect.bind_s(getConfig.ldapcred, getConfig.ldappass)
   search_filter = "(objectclass=*)"
   try:
-    result = connect.search_s(user_ou,ldap.SCOPE_SUBTREE,search_filter,['uid', 'mail'])
-    print(result)
+    result = connect.search_s(getConfig.user_ou,ldap.SCOPE_SUBTREE,search_filter,['uid'])
     connect.unbind_s()
   except ldap.LDAPError as e:
     connect.unbind_s()
@@ -65,7 +42,8 @@ def api_getalluser():
     for dn,entry in result:
       for key,value in entry.items():
         entry[key] = ",".join(value)
-    return jsonify("{{users: {}}}".format(result))
+    #return jsonify("{{users: {}}}".format(result))
+    return jsonify(result[1:])
 
 if __name__ == '__main__':
     app.run(debug=True)
