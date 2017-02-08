1d6150a5664d22bf8730a32ea7283f4bc183da67
aldevar <aldevar@alaben.net>
2017-02-08 22:51:16 +0100

    Better JSON rendering using Python dict

--- webapp.py
+++ webapp.py
@@ -16,7 +16,9 @@ def api_getUser(username):
     for dn,entry in result:
         for key,value in entry.items():
             entry[key] = ",".join(value)
-    return jsonify(result[0])
+    dictresult = {}
+    dictresult[result[0][0]] = result[0][1]
+    return jsonify(dictresult)
 
 
 #Endpoint returning all the users
@@ -26,10 +28,16 @@ def api_getUser(username):
 def api_getAllUser():
     search_filter = "(objectclass=*)"
     result = ldapConn(getConfig.user_ou,search_filter,[getConfig.user_attr])
-    for dn,entry in result:
-        for key,value in entry.items():
-            entry[key] = ",".join(value)
-    return jsonify(result[1:])
+    sorted_result = sorted(result, key=lambda x: x[1])
+    print sorted_result
+    dictresult = {}
+    for elem in result[1:]:
+        entry = {}
+        dn = elem[0]
+        for key,value in elem[1].items():
+            entry[key] = value[0]
+        dictresult[dn] = entry
+    return jsonify(dictresult)
 
 #Endpoint returning every attributes for asked group
 @app.route('/api/v1/group/<groupname>/', methods=['GET'])
@@ -39,17 +47,25 @@ def api_getGroup(groupname):
     for dn,entry in result:
         for key,value in entry.items():
             entry[key] = ",".join(value)
-    return jsonify(result[0])
+    dictresult = {}
+    dictresult[result[0][0]] = result[0][1]
+    return jsonify(dictresult)
 
 #Endpoint returning all the groups
 @app.route('/api/v1/groups/all/', methods=['GET'])
 def api_getAllGroups():
     search_filter = "(objectclass=*)"
     result = ldapConn(getConfig.group_ou,search_filter,[getConfig.group_attr])
-    for dn,entry in result:
-        for key,value in entry.items():
-            entry[key] = ",".join(value)
-    return jsonify(result[1:])
+    sorted_result = sorted(result, key=lambda x: x[1])
+    dictresult = {}
+    for elem in result:
+        entry = {}
+        if elem[0].startswith("cn"):
+            dn = elem[0]
+            for key,value in elem[1].items():
+                entry[key] = value[0]
+            dictresult[dn] = entry
+    return jsonify(dictresult)
 
 #Endoint returning list of users in group
 @app.route('/api/v1/members/<groupname>/', methods=['GET'])
@@ -74,8 +90,9 @@ def api_getMemberOf(username):
     for dn,entry in result:
     #    for key,value in entry.items():
         output.append(entry[getConfig.group_attr][0])
-    return jsonify("memberof",output)
-
+    dictresult = {}
+    dictresult["memberof"] = output
+    return jsonify(dictresult)
 
 if __name__ == '__main__':
     app.run(debug=True)
