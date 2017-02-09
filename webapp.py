9e48ec5949e32b0c4c0e2924bc2e3d0b059420de
aldevar <aldevar@alaben.net>
2017-02-09 22:22:28 +0100

    Pagination for /users/all/ endpoint

--- webapp.py
+++ webapp.py
@@ -22,22 +22,37 @@ def api_getUser(username):
 
 
 #Endpoint returning all the users
-## TODO : Paginate
-## Result is to big to be shown in a single request
+#Returns paginated results with optional parameters
+# limit: number of result to return (default to max)
+# offset: index of the first result (default to 1)
+# ie /api/V1/users/all/?offset=1&limit=50
+# TODO : Add first, next, previous, last page in header
+# TODO : Partial Content Header
 @app.route('/api/v1/users/all/', methods=['GET'])
 def api_getAllUser():
     search_filter = "(objectclass=*)"
     result = ldapConn(getConfig.user_ou,search_filter,[getConfig.user_attr])
     sorted_result = sorted(result, key=lambda x: x[1])
-    print sorted_result
     dictresult = {}
-    for elem in result[1:]:
+    #for elem in result[1:]:
+    for elem in sorted_result[1:]:
         entry = {}
         dn = elem[0]
         for key,value in elem[1].items():
             entry[key] = value[0]
         dictresult[dn] = entry
-    return jsonify(dictresult)
+    total_range = len(dictresult.keys())
+    range_start = request.args.get('offset', 1)
+    range_start = int(range_start) - 1
+    range_limit = request.args.get('limit', total_range)
+    range_end = range_start + int(range_limit)
+    # Case Sensitive sorted results
+    req_range = {k: dictresult[k] for k in
+                 sorted(dictresult.keys())[range_start:range_end]}
+                 # this sorted code tries to sort case insensitive
+                 # Not working well
+                 #sorted(dictresult.keys(), key=lambda y: y.lower())[range_start:range_end]}
+    return jsonify(req_range)
 
 #Endpoint returning every attributes for asked group
 @app.route('/api/v1/group/<groupname>/', methods=['GET'])
